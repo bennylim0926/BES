28a33c1fc9e97c8b27f9cc992bc3bd3ef1bf1382
Benny Lim <bennylim0926@gmail.com>
2025-08-26 12:20:56 +0800

    Dynamically update each category count, remove hardcoded genre and columns read

--- BES/src/main/java/com/example/BES/services/GoogleSheetService.java
+++ BES/src/main/java/com/example/BES/services/GoogleSheetService.java
@@ -3,8 +3,13 @@ package com.example.BES.services;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.BiConsumer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,41 +21,122 @@ import com.google.api.services.sheets.v4.model.ValueRange;
 
 @Service
 public class GoogleSheetService {
+    // if any new category just add here and update the constructor to map the function
+    // need to update dto as well
+    private final static String KEYWORD = "Categories";
+    private final static String POPPING = "Popping";
+    private final static String WAACKING = "Waacking";
+    private final static String LOCKING = "Locking";
+    private final static String BREAKING = "Breaking";
+    private final static String HIPHOP = "Hiphop";
+    private final static String OPEN = "Open";
+    private final static String AUDIENCE = "Audience";
+    
     @Autowired
     GoogleSheetConfig config;
 
-    List<String> ranges = Arrays.asList("F:F", "H:H");
+    Map<String, BiConsumer<GoogleSheetFileDto, List<String>>> actions;
+    List<String> genres;
+
+    public GoogleSheetService(){
+        genres = new ArrayList<>(Arrays.asList(POPPING, WAACKING, LOCKING, BREAKING, HIPHOP, OPEN));
+        actions = new HashMap<>();
+        actions.put(POPPING, (dto,list) -> dto.setPopping(categoriesCount(list, POPPING)));
+        actions.put(WAACKING, (dto,list) -> dto.setWaacking(categoriesCount(list, WAACKING)));
+        actions.put(LOCKING, (dto,list) -> dto.setLocking(categoriesCount(list, LOCKING)));
+        actions.put(BREAKING, (dto,list) -> dto.setBreaking(categoriesCount(list, BREAKING)));
+        actions.put(HIPHOP, (dto,list) -> dto.setHiphop(categoriesCount(list, HIPHOP)));
+        actions.put(OPEN, (dto,list) -> dto.setOpen(categoriesCount(list, OPEN)));
+        actions.put(AUDIENCE, (dto,list) -> dto.setAudience(categoriesCount(list, AUDIENCE)));
+    }
+    
+    // This is hardcoded we cannot confirmed where it will be located
+    /* Correct order should be: 
+     * 1. Search first row to identify the index of rows that has certain keyword eg. Category
+     * 2. Store the indices in list, and by going through the list transform index to alphabet and in H:H format
+     * 3. 
+    */
+    
+    private static String colIndexToLetter(int index) {
+        StringBuilder result = new StringBuilder();
+        while (index > 0) {
+            int rem = (index - 1) % 26;
+            result.insert(0, (char) ('A' + rem));
+            index = (index - 1) / 26;
+        }
+        return result.toString();
+    }
+    
     public GoogleSheetFileDto getSheetInformationById(String fileId) throws IOException{
         GoogleSheetFileDto dto = new GoogleSheetFileDto();
+        // Get the header
+        ValueRange firstRow = config
+                                .getSheets()
+                                .spreadsheets()
+                                .values()
+                                .get(fileId, "1:1")
+                                .execute();
+ 
+        List<String> headers = firstRow.getValues().get(0)
+                                .stream()
+                                .map(object -> Objects.toString(object, null))
+                                .toList();
+
+        // Identify the Alphabet of columns that have "Categories"
+        List<String> matchingColumnIndices = new ArrayList<>();
+        for(int i = 0; i < headers.size(); i++){
+            if(headers.get(i).equalsIgnoreCase(KEYWORD)){
+                String colLetter = colIndexToLetter(i + 1);
+                matchingColumnIndices.add(colLetter + ":" + colLetter);
+            }
+        }                          
+        
+        // Get the value based on the columns identified
+        // usually there will be only two which are local and overseas 
         BatchGetValuesResponse response = config
                                 .getSheets()
                                 .spreadsheets()
                                 .values()
                                 .batchGet(fileId)
-                                .setRanges(ranges)
-                                // If I am the one manage the form is always gonna be this
-                                // .get(fileId, "F:F")
+                                .setRanges(matchingColumnIndices)
                                 .execute();
 
         List<ValueRange> valueRanges = response.getValueRanges();
 
-        List<List<Object>> colF = valueRanges.get(0).getValues();
-        List<List<Object>> colH = valueRanges.get(1).getValues();
+        List<List<Object>> localGenre = valueRanges.get(0).getValues();
+        List<List<Object>> overseasGenre = valueRanges.get(1).getValues();
         List<String> combined = new ArrayList<>();
-        for(int i = 1; i < colF.size(); i++){
-            // combined.addAll(colF.get(i));
-            // combined.get(i).addAll(colH.get(i));
-            if(colF.get(i).size() == 0){
-                combined.add(colH.get(i).get(0).toString());
+        for(int i = 1; i < localGenre.size(); i++){
+            if(localGenre.get(i).size() == 0){
+                combined.add(overseasGenre.get(i).get(0).toString());
             }else{
-                combined.add(colF.get(i).get(0).toString());
+                combined.add(localGenre.get(i).get(0).toString());
             }
         }
-        dto.setPopping(Collections.frequency(combined, "Popping 1v1"));
-        dto.setWaacking(Collections.frequency(combined, "Waacking 1v1"));
-        dto.setLocking(Collections.frequency(combined, "Locking 1v1"));
-        dto.setOpen(Collections.frequency(combined, "Open 1v1"));
-        dto.setAudience(Collections.frequency(combined, "Audience"));
+        setDtoCategory(dto, combined);
         return dto;
+    }
+
+    private Integer categoriesCount(List<String> data, String Category){
+        return (int)data.stream()
+                .filter(s -> s.startsWith(Category))
+                .count();
+    }
+
+    private void setDtoCategory(GoogleSheetFileDto dto, List<String> data){
+        Set<String> categories = new HashSet<String>(data);
+        for (String category : categories){
+            String normalizeCategory = normalizeGenre(category, genres);
+            actions.get(normalizeCategory).accept(dto, data);;
+        }
+    }
+
+    private String normalizeGenre(String event, List<String> genres){
+        for(String genre : genres){
+            if(event.contains(genre)){
+                return genre;
+            }
+        }
+        return event;
     }
 }
