d56e0aee78706130d8664da2001e67bb8f4c1990
Benny Lim <bennylim0926@gmail.com>
2026-05-28 00:21:40 +0800

    fix(battle): add @JsonProperty to SetBattleScoreDto.isFinal to prevent Jackson key stripping

--- BES/src/main/java/com/example/BES/dtos/battle/SetBattleScoreDto.java
+++ BES/src/main/java/com/example/BES/dtos/battle/SetBattleScoreDto.java
@@ -1,6 +1,9 @@
 package com.example.BES.dtos.battle;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class SetBattleScoreDto {
+    @JsonProperty("isFinal")
     private boolean isFinal;
     public boolean isFinal() { return isFinal; }
 }
